7dac5467d669e9de5d899c50337b12ae504abd28
javruifer1 <89843770+javruifer1@users.noreply.github.com>
2023-05-10 11:45:48 +0200

    commit de jose fran y javi

--- src/main/java/aiss/gitminer/controller/IssueController.java
+++ src/main/java/aiss/gitminer/controller/IssueController.java
@@ -1,13 +1,11 @@
 package aiss.gitminer.controller;
 
+import aiss.gitminer.model.Comment;
 import aiss.gitminer.model.Issue;
 import aiss.gitminer.model.Project;
 import aiss.gitminer.repository.IssueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,4 +31,15 @@ public class IssueController {
         Optional<Issue> issue = repository.findById(id);
         return issue.get();
     }
+    @GetMapping("/{id}/comments")
+    public List<Comment> findComment(@PathVariable String id){
+        Optional<Issue> issue = repository.findById(id);
+        return issue.get().getComments();
+    }
+    @GetMapping("issue?/{authorId}")
+    public List<Issue> findIssueByAuthorId(@RequestParam(name = "authorId") String AuthorId ) {
+        List<Issue> issues = repository.findAll();
+        return issues.filter(x -> x.getAuthor = AuthorId);
+    }
+
 }
