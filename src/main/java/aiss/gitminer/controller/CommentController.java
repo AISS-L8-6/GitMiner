4f35ae417e9723766a0864e81724fb8efa09f22e
javruifer1 <89843770+javruifer1@users.noreply.github.com>
2023-05-10 10:39:38 +0200

    comit de jose y mio

--- src/main/java/aiss/gitminer/controller/CommentController.java
+++ src/main/java/aiss/gitminer/controller/CommentController.java
@@ -1,13 +1,16 @@
 package aiss.gitminer.controller;
 
 import aiss.gitminer.model.Comment;
+import aiss.gitminer.model.Project;
 import aiss.gitminer.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/gitminer/comments")
@@ -22,5 +25,11 @@ public class CommentController {
     @GetMapping
     public List<Comment> findAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Comment findOne(@PathVariable String id){
+        Optional<Comment> comment = repository.findById(id);
+        return comment.get();
     }
 }
--- src/main/java/aiss/gitminer/controller/IssueController.java
+++ src/main/java/aiss/gitminer/controller/IssueController.java
@@ -1,13 +1,16 @@
 package aiss.gitminer.controller;
 
 import aiss.gitminer.model.Issue;
+import aiss.gitminer.model.Project;
 import aiss.gitminer.repository.IssueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/gitminer/issue")
@@ -23,5 +26,11 @@ public class IssueController {
     @GetMapping
     public List<Issue> findAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Issue findOne(@PathVariable String id){
+        Optional<Issue> issue = repository.findById(id);
+        return issue.get();
     }
 }
