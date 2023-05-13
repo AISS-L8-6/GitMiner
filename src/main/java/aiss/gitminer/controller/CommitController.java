d6f4d816a8fb724f7561e9086ce64bfb247ac8d3
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2023-05-13 13:35:50 +0200

    CommitController

--- src/main/java/aiss/gitminer/controller/CommitController.java
+++ src/main/java/aiss/gitminer/controller/CommitController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/gitminer/")
+@RequestMapping("/gitminer")
 public class CommitController {
 
     @Autowired
@@ -45,13 +45,41 @@ public class CommitController {
         return result.get();
     }
 
-    //POST HTTP://LOCALHOST:8080/GITMINER/PROJECTS/
+    //POST HTTP://LOCALHOST:8080/GITMINER/PROJECTS/{id}
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/projects/{id}")
     public Commit create(@Valid @RequestBody Commit commit,@PathVariable String projectId) {
         Commit result = repository
                 .save(new Commit(commit.getId(), commit.getTitle(), commit.getMessage(), commit.getAuthorName(), commit.getAuthorEmail(), commit.getAuthoredDate(), commit.getCommitterName(), commit.getCommitterEmail(), commit.getCommittedDate(), commit.getWebUrl()));
         projectRepository.findById(projectId).get().getCommits().add(result);
+        repository.save(result);
         return result;
+    }
+
+    //PUT HTTP://LOCALHOST:8080/GITMINER/COMMITS/{ID}
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/commits/{id}")
+    public void update(@Valid @RequestBody Commit updatedCommit, @PathVariable String id){
+        Optional<Commit> commitData = repository.findById(id);
+        Commit _commit = commitData.get();
+        _commit.setTitle(updatedCommit.getTitle());
+        _commit.setMessage(updatedCommit.getMessage());
+        _commit.setAuthorName(updatedCommit.getAuthorName());
+        _commit.setAuthorEmail(updatedCommit.getAuthorEmail());
+        _commit.setAuthoredDate(updatedCommit.getAuthoredDate());
+        _commit.setCommitterName(updatedCommit.getCommitterName());
+        _commit.setCommitterEmail(updatedCommit.getCommitterEmail());
+        _commit.setCommittedDate(updatedCommit.getCommittedDate());
+        _commit.setWebUrl(updatedCommit.getWebUrl());
+        repository.save(_commit);
+    }
+
+    //DELETE HTTP://LOCALHOST:8080/GITMINER/COMMITS/{ID}
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/commits/{id}")
+    public void delete(@PathVariable String id){
+        if (repository.existsById(id)){
+            repository.deleteById(id);
+        }
     }
 }
