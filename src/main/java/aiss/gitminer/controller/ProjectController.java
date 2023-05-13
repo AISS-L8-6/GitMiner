4b56c6bc2bcabf4904364051af9c0bc6e9c7b5a2
Jorge Gómez de Tovar <jorgomde@alum.us.es>
2023-05-13 14:35:06 +0200

    ProjectController

--- src/main/java/aiss/gitminer/controller/ProjectController.java
+++ src/main/java/aiss/gitminer/controller/ProjectController.java
@@ -48,25 +48,27 @@ public class ProjectController {
         return project.get();
     }
 
-/*
+
     //PUT HTTP/LOCALHOST:8080/PROJECTS/ID
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("{/id}")
-    public void update(@Valid @RequestBody Project updatedProject, @PathVariable long id) {
+    public void update(@Valid @RequestBody Project updatedProject, @PathVariable String id) {
         Optional<Project> projectData = repository.findById(id);
         Project _project = projectData.get();
         _project.setName(updatedProject.getName());
-        //_project.setDescription(updatedProject.getDescription());
+        _project.setWebUrl(updatedProject.getWebUrl());
+        _project.setCommits(updatedProject.getCommits());
+        _project.setIssues(updatedProject.getIssues());
         repository.save(_project);
     }
 
     //DELETE HTTP/LOCALHOST:8080/PROJECTS/ID
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("{/id}")
-    public void update(@PathVariable long id) {
+    public void update(@PathVariable String id) {
         if(repository.existsById(id)){
             repository.deleteById(id);
         }
     }
- */
+
 }
